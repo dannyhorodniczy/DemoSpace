b22ff8bf33732d784f34fff3753afecaa20b4203
Danny Horodniczy <horodniczy.danny@gmail.com>
2024-10-17 16:14:33 -0400

    - bad algo

--- Tests/LeetCode/Medium/JumpGame.cs
+++ Tests/LeetCode/Medium/JumpGame.cs
@@ -1,5 +1,4 @@
 using FluentAssertions;
-using System;
 using System.Linq;
 using Xunit;
 
@@ -8,16 +7,6 @@ namespace Tests.LeetCode.Medium;
 // https://leetcode.com/problems/jump-game/
 public class JumpGame
 {
-    [Theory]
-    [InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
-    [InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
-    [InlineData(new int[] { 2, 0 }, true)]
-    [InlineData(new int[] { 2, 5, 0, 0 }, true)]
-    public void Given_WhenCanJump_Then(int[] nums, bool expected)
-    {
-        bool result = CanJump(nums);
-        result.Should().Be(expected);
-    }
 
     /* 
      * you need a total jump of nums.Length-1
@@ -25,31 +14,71 @@ public class JumpGame
      * 2. determine the values of the possible landing spots
      * 3. make each possible jump (unless you land on 0, or you are >= nums.Length-1)
      * 4. then repeat for each possibility until >= nums.Length-1
+     * 
+     * so this algorithm will work, but it is slow
+     * I think it's very important to have a proper algorithm thought out before implementation
+     * so in this case, the logic was "let's brute force through a potentially *VERY* large tree
+     * for perhaps 1 branch that will work
+     * 
+     * I think better logic is, let's try to get to the start from the end
+     * because if a branch exists, we should find it sooner
      */
 
-    public bool CanJump(int[] nums)
-    {
-        int distanceToGo = nums.Length - 1;
 
-        return CanJumpInternal(nums, 0, distanceToGo);
+    //[Theory]
+    //[InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
+    //[InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
+    //[InlineData(new int[] { 2, 0 }, true)]
+    //[InlineData(new int[] { 2, 5, 0, 0 }, true)]
+    //public void Given_WhenCanJump_Then(int[] nums, bool expected)
+    //{
+    //    bool result = CanJump(nums);
+    //    result.Should().Be(expected);
+    //}
+
+    //public bool CanJump(int[] nums)
+    //{
+
+    //}
+
+    [Theory]
+    [InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
+    [InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
+    [InlineData(new int[] { 2, 0 }, true)]
+    [InlineData(new int[] { 2, 5, 0, 0 }, true)]
+    public void Given_WhenCanJumpBad_Then(int[] nums, bool expected)
+    {
+        bool result = CanJumpBad(nums);
+        result.Should().Be(expected);
     }
 
-    private static bool CanJumpInternal(int[] nums, int index, int distanceToGo)
+    public bool CanJumpBad(int[] nums)
     {
-        if (distanceToGo < 1 || nums[index] >= distanceToGo)
+        int distanceToGo = nums.Length - 1;
+        if (distanceToGo < 1)
         {
             return true;
         }
 
-        int[] range = GetJumpRange(nums[index]);
-        foreach (var i in range)
+        return CanJumpBadInternal(nums, 0, distanceToGo);
+    }
+
+    private static bool CanJumpBadInternal(int[] nums, int index, int distanceToGo)
+    {
+        if (nums[index] >= distanceToGo)
+        {
+            return true;
+        }
+
+        if (nums[index] < 1)
+        {
+            return false;
+        }
+
+        foreach (var i in Enumerable.Range(1, nums[index]))
         {
             // i represents another range of jumps
-            if (CanJumpInternal(nums, i + index, distanceToGo - i) == false)
-            {
-                continue;
-            }
-            else
+            if (CanJumpBadInternal(nums, i + index, distanceToGo - i))
             {
                 return true;
             }
@@ -57,14 +86,4 @@ public class JumpGame
 
         return false;
     }
-
-    private static int[] GetJumpRange(int index)
-    {
-        return index == 0 ? Array.Empty<int>() : Enumerable.Range(1, index).Reverse().ToArray();
-    }
-
-    //private static int[] GetNextLandingSpot(int[] nums, int index)
-    //{
-    //    return index == 0 ? Array.Empty<int>() : Enumerable.Range(1, index).Reverse().ToArray();
-    //}
 }
