f030c57a53e51721a6ba5aaf39f02bc6f332e277
Danny Horodniczy <horodniczy.danny@gmail.com>
2024-10-29 20:11:18 -0400

    - add edge case

--- Tests/LeetCode/Hard/RobotRoomCleaner.cs
+++ Tests/LeetCode/Hard/RobotRoomCleaner.cs
@@ -42,6 +42,27 @@ public class RobotRoomCleaner
         wallCountBefore.Should().Be(wallCountAfter);
     }
 
+    [Fact]
+    public void GivenAnEdgeCase_WhenCleanRoom_ThenTheRoomIsCleaned()
+    {
+        // Given
+        var roomba = new Robot([[1]], 0, 0);
+        int dirtyCountBefore = roomba.RoomState.SelectMany(x => x).Where(x => x == 1).Count();
+        int wallCountBefore = roomba.RoomState.SelectMany(x => x).Where(x => x == 0).Count();
+
+        // When
+        CleanRoom(roomba);
+
+        // Then
+        roomba.RoomState.Any(x => x.Any(y => y == 1)).Should().BeFalse();
+
+        int cleanCountAfter = roomba.RoomState.SelectMany(x => x).Where(x => x == 2).Count();
+        int wallCountAfter = roomba.RoomState.SelectMany(x => x).Where(x => x == 0).Count();
+
+        dirtyCountBefore.Should().Be(cleanCountAfter);
+        wallCountBefore.Should().Be(wallCountAfter);
+    }
+
     private static void CleanRoom(IRobot roomba)
     {
         RoombaDfs(roomba, new HashSet<(int, int)>(), 0, 0, 0);
