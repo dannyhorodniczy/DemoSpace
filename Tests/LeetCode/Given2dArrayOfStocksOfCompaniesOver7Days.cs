ce9901c938692742fe6547cda8dbc6bbc54ea946
Danny Horodniczy <horodniczy.danny@gmail.com>
2024-10-31 10:28:44 -0400

    - from glassdoor

---
+++ Tests/LeetCode/Given2dArrayOfStocksOfCompaniesOver7Days.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests.LeetCode;
+
+// given a 2d array of prices of stocks of different company over 7 days. Find the 3 top company with the highest averages.
+
+public class Given2dArrayOfStocksOfCompaniesOver7Days
+{
+    [Fact]
+    public void Given_When_Then()
+    {
+        var stocks = BuildStocksArray();
+
+        (int company, double avg)[] companyAverages = new (int company, double avg)[stocks.Length];
+
+        for (int i = 0; i < stocks.Length; i++)
+        {
+            companyAverages[i] = (i, stocks[i].Average());
+            Console.WriteLine($"Company: {companyAverages[i].company}, Avg: {companyAverages[i].avg}");
+        }
+
+        Console.WriteLine();
+
+        var ordered = companyAverages.OrderByDescending(x => x.avg).ToArray();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Company: {ordered[i].company}, Avg: {ordered[i].avg}");
+        }
+    }
+
+    private static int[][] BuildStocksArray(int companyCount = 20)
+    {
+        Random rnd = new Random();
+        int[][] stocks = new int[companyCount][]; // [company, day_of_week]
+        for (int i = 0; i < stocks.Length; i++)
+        {
+            stocks[i] = new int[7];
+            for (int j = 0; j < stocks[0].Length; j++)
+                stocks[i][j] = rnd.Next(1, 100);
+        }
+        return stocks;
+    }
+}
