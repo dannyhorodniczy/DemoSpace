1e78c0dda58cdbaa8ba7331f9327493e2afffb09
Danny Horodniczy <horodniczy.danny@gmail.com>
2025-01-21 14:23:45 -0500

    - moar practice

--- Tests/LeetCode/Easy/RemoveDuplicatesFromASortedArray.cs
+++ Tests/LeetCode/Easy/RemoveDuplicatesFromASortedArray.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Tests.LeetCode.Easy;
@@ -22,6 +23,24 @@ public class RemoveDuplicatesFromASortedArray
         nums[..usefulDigitCount].Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
     }
 
+    private static int RemoveDuplicates(int[] nums)
+    {
+        var list = new List<int> { nums[0] };
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (list[^1] == nums[i])
+            {
+                continue;
+            }
+
+            list.Add(nums[i]);
+        }
+
+        Array.Copy(list.ToArray(), nums, list.Count);
+        return list.Count;
+    }
+
     [Theory]
     [InlineData(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 }, 4, new int[] { 0, 1, 2, 3 })]
     [InlineData(new int[] { 1, 1, 1, 2, 2, 3 }, 3, new int[] { 1, 2, 3 })]
@@ -37,7 +56,7 @@ public class RemoveDuplicatesFromASortedArray
         nums[..usefulDigitCount].Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
     }
 
-    private static int RemoveDuplicates(int[] nums)
+    private static int RemoveDuplicatesLessGood(int[] nums)
     {
         if (nums.Length < 2)
         {
@@ -56,7 +75,22 @@ public class RemoveDuplicatesFromASortedArray
         return usefulDigitCount;
     }
 
-    public int RemoveDuplicatesLessGood(int[] nums)
+    [Theory]
+    [InlineData(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 }, 4, new int[] { 0, 1, 2, 3 })]
+    [InlineData(new int[] { 1, 1, 1, 2, 2, 3 }, 3, new int[] { 1, 2, 3 })]
+    [InlineData(new int[] { 1, 1, 2 }, 2, new int[] { 1, 2 })]
+    [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5, new int[] { 0, 1, 2, 3, 4 })]
+    [InlineData(new int[] { 1, 1, 2, 2 }, 2, new int[] { 1, 2 })]
+    [InlineData(new int[] { 1, 1, 1, 1 }, 1, new int[] { 1 })]
+    [InlineData(new int[] { -1, 0, 0, 0, 0, 3, 3 }, 3, new int[] { -1, 0, 3 })]
+    public void Given_WhenRemoveDuplicatesLessLessGood_Then(int[] nums, int usefulDigitCount, int[] expected)
+    {
+        int result = RemoveDuplicatesLessLessGood(nums);
+        result.Should().Be(usefulDigitCount);
+        nums[..usefulDigitCount].Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+    }
+
+    public int RemoveDuplicatesLessLessGood(int[] nums)
     {
         int terminationCount = nums.Length;
 
