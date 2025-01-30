358c255615662a0a35c75bcfa9e4cfc33f655cc3
Danny Horodniczy <horodniczy.danny@gmail.com>
2025-01-30 15:47:31 -0500

    - add URL

--- Tests/LeetCode/Easy/FinalPricesWithASpecialDiscountInAShop.cs
+++ Tests/LeetCode/Easy/FinalPricesWithASpecialDiscountInAShop.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using Xunit;
 
 namespace Tests.LeetCode.Easy;
+
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
 public class FinalPricesWithASpecialDiscountInAShop
 {
     [Theory]
